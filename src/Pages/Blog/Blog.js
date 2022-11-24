f0285f178f6ed97971dad13c7f0400cb3d8b6da8
srbony <srabonycse@gmail.com>
2022-11-24 18:01:27 +0600

    added different ways manage state answer

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>This is blog</h2>
+        <div className='grid '>
+            <div className=' rounded-md bg-slate-500 p-8  my-5'>
+                <div className='text-white'>
+                    <h2 className=''>Q:What are the different ways to manage a state in a react application?</h2>
+                    <p>1.Local state:Local (UI) state Local state is data we manage in one or another component.</p>
+                    <p>2.Global state:Global (UI) state  Global state is data we manage across multiple components.</p>
+                    <p>3.Server state:Server state Data that comes from an external server that must be integrated with our UI state.</p>
+                    <p>4.URL state:URL state Data that exists on our URLs, including the pathname and query parameters.</p>
+                </div>
+
+            </div>
+            <div className='rounded-md bg-slate-500 p-8  my-5'>
+                <div>
+                    <h2>How does prototypical inheritance work?</h2>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
 
 export default Blog;
+
